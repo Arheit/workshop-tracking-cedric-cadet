46a89c4ee840683af947ad3e09b7fa5ca04354de
Arheit <cedric1.cadet@epitech.eu>
2021-06-11 15:58:59 +0400

    step2

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python3
 
 import cv2
-#Open camera
-img = cv2.imread("fischl_see_this_shit.png")
 
-while True:
-    cv2.imshow(("Fischl!"), img)
-    if cv2.waitKey(33) == ord("q"):
-        break
+def open_image():
+    img = cv2.imread("fischl_see_this_shit.png")
+    while True:
+        cv2.imshow(("Fischl!"), img)
+        if cv2.waitKey(33) == ord("q"):
+            break
+    cv2.destroyAllWindows()
 
-cv2.destroyAllWindows()
-#     #Check error
-#     if not [...]:
-#         print("Unable to open camera\n")
-#         break
-#     #Show camera
-#     cv2.[...]("Name", [...])
-#     #Close the window
-#     if cv2.waitKey([...]) = ord([...]):
-#     cv2.destroyAllWindows()
+def open_camera():
+    cam = cv2.VideoCapture(0)
+    while True:
+        ret, frame = cam.read()
+    #Check error
+        if not ret:
+            print("Unable to open camera\n")
+            break
+        #Show camera
+        cv2.imshow("idk", frame)
+        #Close the window
+        if cv2.waitKey(33) == ord("q"):
+            break
+    cv2.destroyAllWindows()
+
+open_camera()

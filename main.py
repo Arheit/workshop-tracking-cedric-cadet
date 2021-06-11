8f4a43444e4ef73aec9358fd98e3f3f0ac8d9170
Arheit <cedric1.cadet@epitech.eu>
2021-06-11 17:03:58 +0400

    bon on sait pas si ca marche ou pas

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import cv2
 import mediapipe as mp
+from pymouse import PyMouse, PyMouseEvent
 
 def open_image():
     img = cv2.imread("fischl_see_this_shit.png")
@@ -15,6 +16,7 @@ def open_camera():
     cam = cv2.VideoCapture(0)
     mpHand = mp.solutions.hands
     hand = mpHand.Hands()
+    m = PyMouse()
     while True:
         ret, frame = cam.read()
     #Check error
@@ -25,6 +27,13 @@ def open_camera():
         results = hand.process(rgb)
         if results.multi_hand_landmarks:
             for handLms in results.multi_hand_landmarks:
+                for id, lm in enumerate(handLms.landmark):
+                    h, w, c = frame.shape
+                    posx, posy = int(lm.x * w), int(lm.y * h)
+                    if id == 4:
+                        fingery = posy
+                        fingerx = posx
+                m.move(fingerx * 4, fingery * 4)
                 mp.solutions.drawing_utils.draw_landmarks(frame, handLms, mpHand.HAND_CONNECTIONS)
         #Show camera
         cv2.imshow("idk", frame)
